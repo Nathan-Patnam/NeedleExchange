e24e925d500d5771f4f4b0c2b39026d7d9171140
CURRYGEDDON <patnamnv@email.wofford.edu>
2017-09-16 11:32:36 -0400

    Send Json Data

--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+
 // This example adds a search box to a map, using the Google Place Autocomplete
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
@@ -7,16 +8,20 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13,
-        mapTypeId: 'roadmap'
-    });
+    var latlng = new google.maps.LatLng(37.09024, -95.712891);
+    var myOptions = {
+        zoom: 5,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+
 
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
 
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
@@ -33,42 +38,30 @@ function initAutocomplete() {
             return;
         }
 
-        // Clear out the old markers.
-        markers.forEach(function(marker) {
-            marker.setMap(null);
-        });
-        markers = [];
+    })
+}
 
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
+function sendAddress(){
+    var userAddress =  $("#pac-input").val();
+    var sendInfo={
+        address:userAddress
+    };
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
+    $.ajax({
+        url: "http://10.194.29.0:9000/find",
+        type: "POST",
+        dataType: "json",
+        success: function (msg) {
+            if (msg) {
+                alert("Somebody" + name + " was added in list !");
+                location.reload(true);
             } else {
-                bounds.extend(place.geometry.location);
+                alert("Cannot add to list !");
             }
-        });
-        map.fitBounds(bounds);
+        },
+
+
+        data: sendInfo
     });
+
 }
